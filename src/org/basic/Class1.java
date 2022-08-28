ab0771cef77f0831318e8edcf330917c1a273758
Loganathanravi <loguaustin7788@gmail.com>
2022-08-28 21:04:34 +0530

    java

---
+++ src/org/basic/Class1.java
@@ -0,0 +1,9 @@
+package org.basic;
+
+                             //CLASS-1
+
+public class Class1 {
+	public void trainer() {
+		System.out.println("trainer name is logu");
+	}
+}
---
+++ src/org/basic/Class2.java
@@ -0,0 +1,14 @@
+package org.basic;
+
+                           //CLASS-2
+
+public class Class2 extends Class1{
+	private void strength() {
+		System.out.println("totally 4 students");
+	}
+      public static void main(String[]args) {
+    	  Class2 c2 = new Class2();
+    	  c2.strength();
+    	  c2.trainer();
+      }
+      }
